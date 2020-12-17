b7cba4bacc595cf7f148a5d28b1436ceafbd83e6
Jack Reed <phillipjreed@gmail.com>
2020-12-17 13:51:03 -0700

    Add thumbnail generation from PDF

---
+++ extract_thumbs.rb
@@ -0,0 +1,20 @@
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+  gem 'rubyzip'
+  gem 'byebug'
+end
+
+puts 'Gems installed and loaded!'
+
+require 'zip'
+
+Dir['assets/tactile_image_files/**/*.pptx'].each do |f|
+  Zip::File.open(f) do |zipfile|
+    file_name = zipfile.name.split('/').last.split('.').first.split('-').first
+    thumbnail = zipfile.find { |v| v.name.start_with? 'docProps/thumbnail' }
+    FileUtils.rm("#{File.expand_path File.dirname(__FILE__)}/assets/thumbnails/#{file_name}.jpeg", force: true)
+    thumbnail.extract("#{File.expand_path File.dirname(__FILE__)}/assets/thumbnails/#{file_name}.jpeg")
+  end
+end
